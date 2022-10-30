a0dc0db4f358a426b59ec46cce62d0033d28d422
martindelacruz <martindelacruz169@gmail.com>
2022-10-29 21:12:42 -0500

    Fix: Se arregló un error en los tokens

--- src/routes/index.js
+++ src/routes/index.js
@@ -80,13 +80,11 @@ function verifyToken(req, res, next) {
     if (!req.headers.authorization) {
         return res.status(401).send('anUthorize Request');
     }
-
     const token = req.headers.authorization.split(' ')[1]
     if (token === 'null') {
         return res.status(401).send('Unathorize Request');
     }
-
     const payload = jwt.verify(token, 'secretkey')
-    req.userId = payload._id
+    req.userId = payload._id;
     next();
 }
